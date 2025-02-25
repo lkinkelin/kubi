907de338f8bb9c8d46c6c1b920251add5115a296
Lancelot KINKELIN <lancelot.kinkelin-prestataire@ca-gip.fr>
2025-02-25 10:17:46 +0100

    add apply rbac in bootstrap script

--- test/e2e/bootstrap-test-e2e.sh
+++ test/e2e/bootstrap-test-e2e.sh
@@ -94,6 +94,7 @@ kubectl apply -f test/e2e/conf/kubi/kube-crds.yml
 kubectl apply -f test/e2e/conf/kubi/kube-prerequisites.yml
 kubectl apply -f test/e2e/conf/kubi/black-white-list-cm.yaml
 kubectl apply -f test/e2e/conf/kubi/kubi-operator-deployment.yaml
+kubectl apply -f test/e2e/conf/kubi/rbac.yaml
 
 ORG=ca-gip goreleaser release --clean --snapshot
 kind load docker-image ghcr.io/ca-gip/kubi-operator:$(git rev-parse --short HEAD)-amd64 --name test-e2e-kubi
